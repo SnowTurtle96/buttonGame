ca8083022c5cc973b8062c72a61fb31f5ca71d25
SnowTurtle96 <jg.clarke@hotmail.co.uk>
2016-12-12 17:50:52 +0000

    Color methods
    Update to color

--- src/snake/Colors.java
+++ src/snake/Colors.java
@@ -3,10 +3,38 @@
  */
 package snake;
 
+import java.awt.Color;
+
 /**
  * @author Jamie
  *
  */
 public class Colors {
 
+	private Color redAlternate;
+	private Color yellowAlternate;
+	private Color greenAlternate;
+	private Color blueAlrernate;
+
+	public Colors() {
+
+	}
+
+	public void changeRedButton() {
+
+		redAlternate = Color.decode("0XFFFFFF");
+
+	}
+
+	public void changeYellowButton() {
+
+	}
+
+	public void changeBlueButton() {
+
+	}
+
+	public void changeGreenButton() {
+
+	}
 }
