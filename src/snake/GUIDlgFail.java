2dae741b112477b9bc5a09f4bde1e793b3313cee
SnowTurtle96 <jg.clarke@hotmail.co.uk>
2016-12-14 03:59:58 +0000

    GUI

--- src/snake/GUIDlgFail.java
+++ src/snake/GUIDlgFail.java
@@ -1,6 +1,10 @@
 package snake;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class GUIDlgFail
@@ -8,16 +12,22 @@ public class GUIDlgFail
 {
 	private JPanel p1;
 	private JFrame window;
+	private JLabel l1;
 
 	public GUIDlgFail() {
 
-		makeFrame();
 		showFrame();
+		makeFrame();
 
 	}
 
 	public void makeFrame() {
-		p1 = new JPanel();
+		p1 = new JPanel(new BorderLayout());
+		l1 = new JLabel();
+		p1.setBackground(Color.black);
+		window.add(p1);
+		l1.setText("Failed!");
+		p1.add(l1, BorderLayout.CENTER);
 
 	}
 
